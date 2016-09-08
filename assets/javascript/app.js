972b39cf87d734d61682486b738dbad21ecf9f29
siddhant <siddhant@172-17-92-237.DYNAPOOL.NYU.EDU>
2016-09-08 17:09:09 -0400

    trivia

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,13 +13,27 @@ var questions = [{
 	answers:["Germany","Brazil","Spain","Argentina"],
 	correctAns:"Spain"
 
+}, {
+	question:"Michael Phelps is the most decorated Olympian of all time, the number of medals won by him?",
+	answers:["24","25","27","28"],
+	correctAns:"28"	
+
+}, {
+	quesiton:"For which movie did Leonardo Di Caprio win an Oscar?",
+	answers:["Titanic","The Revenant","The Wolf of Wall Street","Blood Diamond"],
+	correctAns:"The Revenant"
+}, {
+	question:"Francis J Underwood played by Kevin Spacey is a character from which popular TV show?",
+	answers:["Mad Men","House of Cards","Reservoir Dogs","Narcos"],
+	correctAns:"House of Cards"
+
 }];
 
 
 var game = {
 	correct: 0,
 	incorrect: 0,
-	counter: 20,
+	counter: 50,
 	countdown: function(){
 		game.counter--;
 		$('#counter').html(game.counter);
@@ -33,7 +47,7 @@ var game = {
 
 		timer = setInterval(game.countdown, 1000);
 
-		$('#subbod').prepend('<h2>Time Remaining: <span id= "counter" > 120 </span> Seconds</h2>');
+		$('#subbod').prepend('<h2>Time Remaining: <span id= "counter" > 50 </span> Seconds</h2>');
 
 		$('#start').remove();
 		for(var i=0; i<questions.length;i++){
@@ -46,11 +60,12 @@ var game = {
 
 			}
 		}	
+		$('#subbod').append('<br> <button id="end" >Done</button');
 	},
 	done: function() {
 
-		$.each($('input[name="question-0]":'),function(){
-			if($(this).val()==questions[0].correctAns){
+		$.each($('input[name="question-0]":checked'), function(){
+			if($(this).val() == questions[0].correctAns){
 				game.correct++;
 			}
 			else{
@@ -58,7 +73,7 @@ var game = {
 			}
 
 			}),	
-			$.each($('input[name="question-1]":'),function(){
+			$.each($('input[name="question-1]":checked'),function(){
 			if($(this).val()==questions[1].correctAns){
 				game.correct++;
 			}
@@ -67,15 +82,50 @@ var game = {
 			}
 
 			
+		}),
+			$.each($('input[name="question-2]":checked'),function(){
+			if($(this).val()==questions[2].correctAns){
+				game.correct++;
+			}
+			else{
+				game.incorrect++;
+			}
+
+			
+		}),
+			$.each($('input[name="question-3]":checked'),function(){
+			if($(this).val()==questions[3].correctAns){
+				game.correct++;
+			}
+			else{
+				game.incorrect++;
+			}
+
+			
+		}),
+			$.each($('input[name="question-4]":checked  '),function(){
+			if($(this).val()==questions[4].correctAns){
+				game.correct++;
+			}
+			else{
+				game.incorrect++;
+			}
+
+			
 		});
+
 		this.result();
+		
 		},
+		
+
 		result: function(){
+			
 			clearInterval(timer);
 
 			$('#subbod h2').remove();
 
-			$('#subbod').hmtl("<h2>All done!</h2>");
+			$('#subbod').hmtl("<h2>Done!</h2>");
 			$('#subbod').append("<h3>Correct Answers: " +this.correct+ "</h3>");
 			$('#subbod').append("<h3>incorrect Answers: " +this.incorrect+ "</h3>" );
 			$('#subbod').append("<h3>Unanswered: " +(questions.length-(this.incorrect+this.correct))+"</h3> ");
@@ -83,4 +133,5 @@ var game = {
 
 		}	
 	}
+
 
